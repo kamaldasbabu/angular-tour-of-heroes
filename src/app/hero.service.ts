a913f4ff749163d5dd9551e67fe1af017600a262
kamal Ruidas <kamaldasbabu@gmail.com>
2021-01-20 19:16:57 +0530

    wednesday commit 2

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,9 @@
 import { MessageService } from './message.service';
 import { Hero } from './hero';
-import { HEROES } from './monk-heroes';
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,16 +13,53 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HeroService {
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
-  private heroesUrl = 'api/heroes';  
+  private heroesUrl = 'api/heroes'; 
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }; 
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
-  getHeroes():Observable<Hero[]>{
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  // Handling Error
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
+
+  getHeroes(): Observable<Hero[]>{
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fatched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
 }
+
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
+import { MessageService } from './../message.service';
 
 import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-heroes',
@@ -21,7 +23,9 @@ export class HeroesComponent implements OnInit {
   //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   // }
   heroes : Hero[];
-  constructor( private heroService: HeroService) { }
+  constructor( private heroService: HeroService,
+    private http: HttpClient,
+    private messageService: MessageService) { }
   
 
   ngOnInit(): void {
@@ -32,5 +36,13 @@ export class HeroesComponent implements OnInit {
       this.heroService.getHeroes()
         .subscribe(heroes =>this.heroes = heroes);
 
+}
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
 }
 }
